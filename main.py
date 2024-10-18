aefa86b04aaabdd69c7768caa49cdf7fbdb36120
JakubPoblocki <jakubpoblocki1@gmail.com>
2024-10-18 22:58:24 +0200

    added required types to variables and required return types for functions

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from easyAI import Negamax, Human_Player, AI_Player
 from models import Chomp
 
 if __name__ == '__main__':
-    ai = Negamax(50)
+    ai = Negamax(13)
     game = Chomp([Human_Player(), AI_Player(ai)])
     history = game.play()
